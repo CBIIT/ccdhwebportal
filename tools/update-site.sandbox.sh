56f6a9c7e9160849de14a0887aef2b371d5ceb87
Debra Venckus <dvenckus@uchicago.edu>
2021-05-25 15:53:33 -0500

    Top-level deployment scripts

---
+++ tools/update-site.sandbox.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# update-site.sh
+#
+
+SITE="sandbox"
+
+# archive the sites dir just in case
+echo "Update CCDH Web Portal site"
+
+echo "Archive sites"
+mkdir -p /var/www/html/sites-archive
+tar -czvf /var/www/html/sites-archive/sites.`date + "%Y%m%d_%H%M%S"`.tgz /var/www/html/ccdhwebportal/web/sites
+
+echo "Update code & run deploy script"
+cd /var/www/html/ccdhwebportal
+git pull
+git reset --hard origin/master
+git pull
+./tools/deploy.sh master $SITE
+
+echo "regenerate sitemap"
+drush -y --uri="https://ccdhportaldev.pedscommons.org" simple-sitemap:generate
+
+echo "Done!"
+
